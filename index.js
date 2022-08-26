74dd97ab12cb2853a62c1b9c5cf92651847a9e9d
Patrick Mason <patrickmason73@gmail.com>
2022-08-25 20:11:55 -0600

    completed

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var re = /[\W_]/g;
+  var lowRegWord = word.toLowerCase().replace(re, '');
+  var reverseWord = lowRegWord.split('').reverse().join(''); 
+  return reverseWord === lowRegWord
 }
 
 /* 
   Add your pseudocode here
+  make all lowercase
+  split word and reverse
+  if equals, it is a palindrome(true)
 */
 
 /*
   Add written explanation of your solution here
+  The function takes a word and splits and reverses it to see if it still equals the original word
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +28,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("mom"));
 }
 
 module.exports = isPalindrome;
